2ff50a18ef35cf53b26e63e8853faf448b0768ab
FabsSeven <75510266+fabricio-neves@users.noreply.github.com>
2022-01-06 04:07:28 +0100

    Update json_url_get_msql_insert.py
    Removing tests.

--- json_url_get_msql_insert.py
+++ json_url_get_msql_insert.py
@@ -17,24 +17,9 @@ engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
 
 try:
     r = requests.get(url)  # getting data
-    # print(r.status_code)  # testing connection
     print(r.raise_for_status())
     jsonResponse = r.json()
-    print('\n Entire JSON response:')
-    print(jsonResponse)
-
-    print('\n Print each key-value pair from JSON response:')
-    for key, value in jsonResponse.items():
-        print(key, ': ', value)
-
-    print("\n Access directly using a JSON key name")
-    print("Data is:")
-    print(jsonResponse["data"])
-
-    print('\n access nested JSON keys:')
-    print('color is')
-    print(jsonResponse["data"][0]["color"])
-
+    
     # Create dataframe
     df = pd.json_normalize(jsonResponse['data'])
     # Convert dataframe to sql table
